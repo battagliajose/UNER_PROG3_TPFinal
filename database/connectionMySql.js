1ac95f6eb4ea6ddf8eb46c045f448f7174e04a77
José Battaglia <battagliajose@gmail.com>
2024-09-12 12:37:58 -0300

    agregado connectionLimit

--- database/connectionMySql.js
+++ database/connectionMySql.js
@@ -4,6 +4,7 @@ import { createPool } from 'mysql2/promise';
 dotenv.config();
 
 export const pool = createPool({
+    connectionLimit: 10,
     host: process.env.HOST,
     port: process.env.MYSQLPORT,
     database: process.env.DATABASE,
