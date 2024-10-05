a57640f041a06c8db511281ef53fba46573e061c
José Battaglia <battagliajose@gmail.com>
2024-10-05 10:48:10 -0300

    Agregado Middleware ContentType

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import oficinasRouter from './v1/routes/oficinasRouter.js'
 import usuariosRouter from './v1/routes/usuariosRouter.js'
+
+import validateContentType from './middlewares/validateContentType.js';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(validateContentType);
 
 //Routes
 app.use('/oficinas', oficinasRouter);
