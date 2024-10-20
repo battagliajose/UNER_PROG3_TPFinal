b9ac9dbed27ba345a69b2124d42cebb4ca4c855e
cristianseltenreich <cristianseltenreich@gmail.com>
2024-10-20 20:27:11 -0300

    se agrega validación de los datos par login

--- src/v1/routes/authRouter.js
+++ src/v1/routes/authRouter.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import AuthController from '../../controllers/authController.js';
 
+import { check } from 'express-validator';
+import { validarCampos } from '../../middlewares/validarCampos.js';
+
 const authRouter = express.Router();
 
 const authController = new AuthController();
 
-authRouter.post('/login', authController.login);
+// Validaciones en Express Validator para email y contraseña. 
+authRouter.post('/login',
+    [
+        check('correoElectronico', 'El correo electrónico es requerido!').not().isEmpty(),
+        check('correoElectronico', 'Por favor, revisá el formato e ingresá un email válido').isEmail(),
+        check('contrasenia', 'La contrasenia es requerida!').not().isEmpty(),
+        validarCampos
+    ], 
+    
+    authController.login);
 
 export default authRouter;
